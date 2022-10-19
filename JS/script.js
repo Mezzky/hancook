74789fe7a7547a58acc2b3f4a995a83da9e2ca8c
Mezzky <ryansahadha@gmail.com>
2022-10-20 02:08:26 +0800

    Add: New Progress

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,26 @@
-// Hamburger
+// Variables
 const hamburger = document.querySelector('#hamburger');
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("header .navbar");
+const header = document.querySelector('header');
 
+
+// Scrolled Animation
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        header.classList.add('scrolled');
+    } else{
+        header.classList.remove('scrolled');
+    }
+});
+
+// Hamburger
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
 });
 
 // Menu Toggle
-// const menuToggle = document.querySelector(".menu-toggle");
-// const nav = document.querySelector("header .navbar");
-
-// menuToggle.addEventListener('click', () => {
-//     nav.classList.toggle('slide');
-// });
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('slide');
+    header.classList.toggle('clicked');
+});
