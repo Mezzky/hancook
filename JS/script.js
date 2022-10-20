9a3cef34974af3ed9dbbffd2d4ad44d43ab76d87
Mezzky <ryansahadha@gmail.com>
2022-10-20 20:47:07 +0800

    Add: New Progress

--- JS/script.js
+++ JS/script.js
@@ -9,10 +9,10 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         header.classList.add('scrolled');
-        // nav.classList.add('scrolled');
+        nav.classList.add('scrolled');
     } else{
         header.classList.remove('scrolled');
-        // nav.classList.remove('scrolled');
+        nav.classList.remove('scrolled');
     }
 });
 
